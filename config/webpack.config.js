74b04c04057cca492e6538ca4370a112cf748aa4
JP Erasmus <jperasmus11@gmail.com>
2017-07-30 23:08:43 +0200

    Add babel, webpack and firebase dependencies

---
+++ config/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: '../src/index.js',
+
+  output: {
+    filename: 'flamelink.js',
+    path: path.resolve(__dirname, '..', 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};

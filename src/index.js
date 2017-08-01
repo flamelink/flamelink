0537f1e55d7369495f6673745f6086bac43bce73
JP Erasmus <jperasmus11@gmail.com>
2017-08-01 10:35:35 +0200

    Add initial unit tests for content and navigation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import * as firebase from 'firebase';
 
+let firebaseApp = null;
+
 export default function(config = {}) {
-  return firebase.initializeApp(config);
+  if (config.firebaseApp) {
+    firebaseApp = config.firebaseApp;
+  } else {
+    firebaseApp = firebaseApp ? firebaseApp : firebase.initializeApp(config);
+  }
+
+  return {
+    firebaseApp,
+    setLanguage() {},
+    setEnv() {},
+    content: {
+      get() {},
+      on() {},
+      off() {},
+      set() {},
+      remove() {}
+    },
+    nav: {
+      get() {},
+      on() {},
+      off() {},
+      set() {},
+      remove() {}
+    }
+  };
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,69 @@
 import flamelink from '../src/index';
 
-test('should expose a function that returns an object when invoked', () => {
-  expect(typeof flamelink).toBe('function');
-  expect(typeof flamelink()).toBe('object');
+describe('Flamelink SDK', () => {
+  test('should expose a factory function', () => {
+    expect(typeof flamelink).toBe('function');
+    expect(typeof flamelink()).toBe('object');
+  });
+
+  test('should expose the `firebaseApp` instance', () => {
+    expect(flamelink().hasOwnProperty('firebaseApp')).toBe(true);
+  });
+
+  test('should expose the `firebaseApp` instance if passed in via config', () => {
+    const fakeFirebaseApp = { key: 'value' };
+    expect(flamelink({ firebaseApp: fakeFirebaseApp }).firebaseApp).toBe(fakeFirebaseApp);
+  });
+
+  test('should expose a "setLanguage" method', () => {
+    expect(flamelink().hasOwnProperty('setLanguage')).toBe(true);
+  });
+
+  test('should expose a "setEnv" method', () => {
+    expect(flamelink().hasOwnProperty('setEnv')).toBe(true);
+  });
+
+  describe('Content', () => {
+    test('should expose a "get" method', () => {
+      expect(flamelink().content.hasOwnProperty('get')).toBe(true);
+    });
+
+    test('should expose a "set" method', () => {
+      expect(flamelink().content.hasOwnProperty('set')).toBe(true);
+    });
+
+    test('should expose a "on" method', () => {
+      expect(flamelink().content.hasOwnProperty('on')).toBe(true);
+    });
+
+    test('should expose a "off" method', () => {
+      expect(flamelink().content.hasOwnProperty('off')).toBe(true);
+    });
+
+    test('should expose a "remove" method', () => {
+      expect(flamelink().content.hasOwnProperty('remove')).toBe(true);
+    });
+  });
+
+  describe('Navigation', () => {
+    test('should expose a "get" method', () => {
+      expect(flamelink().nav.hasOwnProperty('get')).toBe(true);
+    });
+
+    test('should expose a "set" method', () => {
+      expect(flamelink().nav.hasOwnProperty('set')).toBe(true);
+    });
+
+    test('should expose a "on" method', () => {
+      expect(flamelink().nav.hasOwnProperty('on')).toBe(true);
+    });
+
+    test('should expose a "off" method', () => {
+      expect(flamelink().nav.hasOwnProperty('off')).toBe(true);
+    });
+
+    test('should expose a "remove" method', () => {
+      expect(flamelink().nav.hasOwnProperty('remove')).toBe(true);
+    });
+  });
 });

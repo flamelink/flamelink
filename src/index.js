f20c9f2cbe6dae751d8a51c6ffb827b7baa7f75d
JP Erasmus <jperasmus11@gmail.com>
2017-08-01 22:00:41 +0200

    Expose `VERSION` and `getRaw` methods

--- src/index.js
+++ src/index.js
@@ -1,20 +1,110 @@
 import * as firebase from 'firebase';
+import pkg from '../package.json';
 
-let firebaseApp = null;
+let fb_ = null;
+let lang_ = '';
+let env_ = '';
 
-export default function(config = {}) {
+function flamelink(config = {}) {
   if (config.firebaseApp) {
-    firebaseApp = config.firebaseApp;
-  } else {
-    firebaseApp = firebaseApp ? firebaseApp : firebase.initializeApp(config);
+    fb_ = config.firebaseApp;
+  } else if (!fb_) {
+    const { apiKey, authDomain, databaseURL, storageBucket } = config;
+
+    if (!apiKey || !authDomain || !databaseURL) {
+      throw new Error('[FLAMELINK] The following config properties are mandatory: "apiKey", "authDomain", "databaseURL"');
+    }
+
+    fb_ = firebase.initializeApp({
+      apiKey: 'AIzaSyAxlh-gBxcRkQWbxC0L10S5Qo3Su6xRs8E',
+      authDomain: 'fir-editor.firebaseapp.com',
+      databaseURL: 'https://fir-editor.firebaseio.com',
+      storageBucket: 'fir-editor.appspot.com'
+    });
   }
 
   return {
-    firebaseApp,
-    setLanguage() {},
-    setEnv() {},
+    firebaseApp: fb_,
+    setLanguage(lang = lang_) {
+      lang_ = lang;
+    },
+    setEnv(env = env_) {
+      env_ = env;
+    },
     content: {
-      get() {},
+      /**
+       * Read value once from db and return raw snapshot
+       *
+       * @param {String} ref
+       * @param {Object} [options={}]
+       * @returns {Promise} Resolves to snapshot of query
+       */
+      getRaw(ref, options = {}) {
+        let orderBy;
+        switch ((options.orderBy || '').toUpperCase()) {
+          case 'CHILD':
+            orderBy = 'orderByChild';
+            break;
+
+          case 'VALUE':
+            orderBy = 'orderByValue';
+            break;
+
+          case 'KEY':
+          default:
+            orderBy = 'orderByKey';
+        }
+
+        return new Promise((resolve, reject) => {
+          fb_
+            .database()
+            .ref(`content${ref ? `/${ref}` : ''}`)
+            // [orderBy]()
+            // .equalTo('1')
+            .once('value')
+            .then(resolve)
+            .catch(reject);
+        });
+      },
+
+      /**
+       * Read value once from db
+       *
+       * @param {String} ref
+       * @param {Object} [options={}]
+       * @returns {Promise} Resolves to value of query
+       */
+      get(ref, options = {}) {
+        let orderBy;
+        switch ((options.orderBy || '').toUpperCase()) {
+          case 'CHILD':
+            orderBy = 'orderByChild';
+            break;
+
+          case 'VALUE':
+            orderBy = 'orderByValue';
+            break;
+
+          case 'KEY':
+          default:
+            orderBy = 'orderByKey';
+        }
+
+        return new Promise((resolve, reject) => {
+          fb_
+            .database()
+            .ref(`content${ref ? `/${ref}` : ''}`)
+            // [orderBy]()
+            // .equalTo('1')
+            .once('value')
+            .then(snapshot => {
+              const value = snapshot.val();
+              console.log(value);
+              resolve(value);
+            })
+            .catch(reject);
+        });
+      },
       on() {},
       off() {},
       set() {},
@@ -29,3 +119,7 @@ export default function(config = {}) {
     }
   };
 }
+
+flamelink.VERSION = pkg.version;
+
+export default flamelink;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,17 +1,59 @@
 import flamelink from '../src/index';
+import pkg from '../package.json';
+
+const fakeFirebaseApp = {
+  firebase_: {
+    __esModule: true,
+    initializeApp: () => {},
+    app: {},
+    apps: () => {},
+    Promise: () => {},
+    SDK_VERSION: '4.2.0',
+    INTERNAL: {},
+    default: {},
+    auth: {},
+    User: {},
+    database: {}
+  },
+  isDeleted_: false,
+  services_: { auth: {} },
+  name_: '[DEFAULT]',
+  options_: {
+    apiKey: 'AIzaSyAxlh-gBxcRkQWbxC0L10S5Qo3Su6xRs8E',
+    authDomain: 'fir-editor.firebaseapp.com',
+    databaseURL: 'https://fir-editor.firebaseio.com',
+    storageBucket: 'fir-editor.appspot.com'
+  },
+  INTERNAL: {
+    getUid: () => {},
+    getToken: () => {},
+    addAuthTokenListener: () => {},
+    removeAuthTokenListener: () => {}
+  }
+};
 
 describe('Flamelink SDK', () => {
-  test('should expose a factory function', () => {
-    expect(typeof flamelink).toBe('function');
-    expect(typeof flamelink()).toBe('object');
+  test('should expose the package version', () => {
+    expect(flamelink.VERSION).toBe(pkg.version);
   });
 
-  test('should expose the `firebaseApp` instance', () => {
-    expect(flamelink().hasOwnProperty('firebaseApp')).toBe(true);
+  test('should expose a factory function', () => {
+    expect(typeof flamelink).toBe('function');
+  });
+
+  test('should throw an error if initialized without the mandatory properties', () => {
+    expect(flamelink).toThrow('[FLAMELINK] The following config properties are mandatory: "apiKey", "authDomain", "databaseURL"');
   });
 
   test('should expose the `firebaseApp` instance if passed in via config', () => {
-    const fakeFirebaseApp = { key: 'value' };
+    expect(
+      flamelink({
+        apiKey: 'AIzaSyAxlh-gBxcRkQWbxC0L10S5Qo3Su6xRs8E',
+        authDomain: 'fir-editor.firebaseapp.com',
+        databaseURL: 'https://fir-editor.firebaseio.com',
+        storageBucket: 'fir-editor.appspot.com'
+      })
+    ).toBeTruthy();
     expect(flamelink({ firebaseApp: fakeFirebaseApp }).firebaseApp).toBe(fakeFirebaseApp);
   });
 
@@ -24,6 +66,10 @@ describe('Flamelink SDK', () => {
   });
 
   describe('Content', () => {
+    test('should expose a "getRaw" method', () => {
+      expect(flamelink().content.hasOwnProperty('getRaw')).toBe(true);
+    });
+
     test('should expose a "get" method', () => {
       expect(flamelink().content.hasOwnProperty('get')).toBe(true);
     });

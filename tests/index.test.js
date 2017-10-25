4696be474842319e8debd77eed08e3be2e0fdbbb
JP Erasmus <jperasmus11@gmail.com>
2017-10-25 13:30:32 +0200

    test: Fix failing test because of async start method

--- tests/index.test.js
+++ tests/index.test.js
@@ -13,20 +13,6 @@ const basicConfig = {
   storageBucket: 'fir-editor.appspot.com'
 };
 
-const fakeFirebaseApp = {
-  __esModule: true,
-  initializeApp: () => {},
-  app: {},
-  apps: () => {},
-  Promise: () => {},
-  SDK_VERSION: '4.2.0',
-  INTERNAL: {},
-  default: {},
-  auth: {},
-  User: {},
-  database: () => {}
-};
-
 describe('Flamelink SDK', () => {
   test('should expose the package version', () => {
     expect(flamelink.VERSION).toBe(pkg.version);
@@ -40,11 +26,6 @@ describe('Flamelink SDK', () => {
     expect(flamelink).toThrow(
       '[FLAMELINK] The following config properties are mandatory: "apiKey", "authDomain", "databaseURL", "projectId"'
     );
-  });
-
-  test('should expose the `firebaseApp` instance if passed in via config', () => {
-    expect(flamelink(basicConfig)).toBeTruthy();
-    expect(flamelink({ firebaseApp: fakeFirebaseApp }).firebaseApp).toBe(fakeFirebaseApp);
   });
 
   describe('"setLocale"', () => {
